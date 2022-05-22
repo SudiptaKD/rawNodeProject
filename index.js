fc24a5bfa74c04c995b1f01b09ff0fc3c901aeb8
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-05-23 02:26:47 +0600

    create data func

--- index.js
+++ index.js
@@ -8,9 +8,15 @@
 const http = require('http');
 const {handleReqRes} = require('./helpers/handleReqRes')
 const environment = require('./helpers/environments')
+const data = require('./lib/data')
 
 //App object - Module scuffolding
 const app ={};
+
+//testing file system
+data.create('test', 'newFile', {'name': 'Bangladesh', 'language':'Bangal'}, (err)=> {
+    console.log('error was', err);
+} )
 
 // create server
 app.createServer = () => {
