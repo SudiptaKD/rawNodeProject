cc9b00dd52152cc61928d975531ed30e529db980
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-05-27 00:25:07 +0600

    get and put checks

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -73,7 +73,7 @@ handler._check.post = (requestProperties, callback) => {
                                 let userChecks = typeof(userObject.checks)=== 'object'
                                                 && userObject.checks instanceof Array 
                                                 ? userObject.checks : [];
-                                                
+
                                 if(userChecks.length < maxChecks) {
                                     let checkId = createRandomString(20);
                                     let checkObject ={
@@ -150,10 +150,142 @@ handler._check.post = (requestProperties, callback) => {
 };
 
 handler._check.get = (requestProperties, callback) => {
+    // check the id is valid
+    const id = typeof(requestProperties.queryStringObject.id) === 'string' 
+            && requestProperties.queryStringObject.id.trim().length === 20
+            ? requestProperties.queryStringObject.id : false;
+
+    if(id) {
+        //look up the checks
+        data.read('checks', id, (err, checkData)=>{
+            let userPhone = parseJSON(checkData).userPhone;
+            if(!err && checkData) {
+                let token = typeof(requestProperties.headersObject.token)==='string' 
+                            ? requestProperties.headersObject.token : false;
+                 
+                //verify the token
+                tokenHandler._token.verify(token, userPhone, (tokenIsValid)=> {
+                    if(tokenIsValid){
+                        callback(200, parseJSON(checkData));
+
+                    } else {
+                        callback(403, {
+                            error : "Authentication Problem!"
+                        })
+                    }
+                })
+
+            }   else {
+                    callback(500, {
+                        error : "There was a problem in the server side!"
+                    })
+            }
+        })
+
+    }   else {
+            callback(400, {
+                error : "You have a problem in your request"
+            })
+    }     
+    
 
 };
 
 handler._check.put = (requestProperties, callback) => {
+    // check the id is valid
+    const id = typeof(requestProperties.body.id) === 'string' 
+            && requestProperties.body.id.trim().length === 20
+            ? requestProperties.body.id : false;
+    
+    //validate inputs
+    let protocol = typeof(requestProperties.body.protocol)==='string'
+                && ['http', 'https'].indexOf(requestProperties.body.protocol) > -1
+                ? requestProperties.body.protocol : false;
+
+    let url = typeof(requestProperties.body.url)==='string'
+            && requestProperties.body.url.trim().length > 0
+            ? requestProperties.body.url : false;
+
+    let method = typeof(requestProperties.body.method)==='string'
+                && ['GET', 'POST', 'PUT', 'DELETE'].indexOf(requestProperties.body.method) > -1
+                ? requestProperties.body.method : false;        
+
+    let successCodes = typeof(requestProperties.body.successCodes)==='object'
+                    && requestProperties.body.successCodes instanceof Array
+                    ? requestProperties.body.successCodes : false;
+
+    let timeoutSeconds = typeof(requestProperties.body.timeoutSeconds)==='number'
+                        && requestProperties.body.timeoutSeconds % 1 === 0
+                        && requestProperties.body.timeoutSeconds >= 1
+                        && requestProperties.body.timeoutSeconds <= 5
+                        ? requestProperties.body.timeoutSeconds : false;
+    
+    if(id){
+        if(protocol || url || method || successCodes || timeoutSeconds) {
+            //check database and get phone number to verify token
+            data.read('checks', id, (err1, checkData)=> {
+                if(!err1 && checkData) {
+                    let checkObject = parseJSON(checkData);
+                    let token = typeof(requestProperties.headersObject.token)==='string' 
+                                ? requestProperties.headersObject.token : false;
+                    
+                    //verify the token
+                    tokenHandler._token.verify(token,checkObject.userPhone, (tokenIsValid)=> {
+                        if(tokenIsValid) {
+                            if(protocol) {
+                                checkObject.protocol =protocol;
+                            }
+                            if(url) {
+                                checkObject.url =url;
+                            }
+                            if(method) {
+                                checkObject.method =method;
+                            }
+                            if(successCodes) {
+                                checkObject.successCodes =successCodes;
+                            }
+                            if(timeoutSeconds) {
+                                checkObject.timeoutSeconds =timeoutSeconds;
+                            }
+
+                            // store  the checkObject
+                            data.update('checks', id, checkObject, (err2)=> {
+                                if(!err2) {
+                                    callback(200)
+
+                                }   else {
+                                        callback(500, {
+                                            error : "There was a problem in the server side!"
+                                        })
+                                }
+                            })
+
+                        }   else {
+                                callback(403, {
+                                    error : "Authentication Problem!"
+                                })
+                        }
+                    })
+
+                }   else {
+                        callback(500, {
+                            error : "There was a problem in the server side!"
+                        })
+                }
+
+            })
+
+        }   else {
+                callback(400, {
+                    error : "You must provide at least one field to update"
+                })
+            }
+
+    }   else {
+            callback(400, {
+                error : "You have a problem in your request"
+            })
+    }                 
 
 };
 
